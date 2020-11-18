3751da7f0748c324627ebacdfbff959007efb96e
belgacem abibakr <belgacemabibakr@gmail.com>
2020-11-18 23:03:46 +0100

    add the <data> element in the detail screen to add the <data> element and do the two way binding on the edit Text

--- app/src/main/java/com/bobabelga/theshoestore/ui/fragments/ShoeDetailFragment.kt
+++ app/src/main/java/com/bobabelga/theshoestore/ui/fragments/ShoeDetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -15,18 +16,21 @@ import com.bobabelga.theshoestore.models.Shoe
 
 
 class ShoeDetailFragment : Fragment() {
-
+    lateinit var binding: FragmentShoeDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val binding: FragmentShoeDetailBinding =
+        binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_detail, container, false)
         var shoe = ShoeDetailFragmentArgs.fromBundle(requireArguments()).shoe
-        var newShoe: Shoe?
-
+        var newShoe = Shoe("", 0.0, "", "")
+        binding.shoe = newShoe
+        binding.sizeEditText.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) binding.sizeEditText.text.clear()
+        }
         binding.saveBtn.setOnClickListener { view: View ->
             if (binding.nameEditText.text.toString().isEmpty())
                 Toast.makeText(context, "Name is empty", Toast.LENGTH_SHORT).show()
@@ -37,21 +41,18 @@ class ShoeDetailFragment : Fragment() {
             else if (binding.descEditText.text.toString().isEmpty())
                 Toast.makeText(context, "Descriptin is empty", Toast.LENGTH_SHORT).show()
             else {
-                newShoe = Shoe(
-                    binding.nameEditText.text.toString(),
-                    binding.sizeEditText.text.toString().toDouble(),
-                    binding.companyEditText.text.toString(),
-                    binding.descEditText.text.toString()
-                )
+                newShoe = binding.shoe as Shoe
                 if (shoe != null) {
-                    shoe!!.shoeArrayList.add(newShoe!!)
+                    shoe!!.shoeArrayList.add(newShoe)
                 } else {
                     shoe = SheoList(ArrayList())
-                    shoe!!.shoeArrayList.add(newShoe!!)
+                    shoe!!.shoeArrayList.add(newShoe)
                 }
                 view.findNavController()
                     .navigate(
-                        ShoeDetailFragmentDirections.actionShoeDetailFragmentToShoeListingFragment(shoe)
+                        ShoeDetailFragmentDirections.actionShoeDetailFragmentToShoeListingFragment(
+                            shoe
+                        )
                     )
             }
 
@@ -64,5 +65,4 @@ class ShoeDetailFragment : Fragment() {
         }
         return binding.root
     }
-
 }
